f6110e398c373b6dd92ea9f02e4ef4b068f23045
Precious Idubor <preciousidubor@gmail.com>
2023-08-03 14:03:35 -0700

    Format method implementation to filter values in incoming log records using filter_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,3 +12,23 @@ def filter_datum(fields: List[str], redaction: str,
     res = re.sub(r'({})=.*?{}'.format(fields, separator),
                  rf"\1={redaction}{separator}", message)
     return res
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialization"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Implement the format method to filter values in
+        incoming log records using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION, super().format(record),
+                            self.SEPARATOR)
