65f94b877188cd9bc2dfe6b872ec6bc67cbdb4c4
Precious Idubor <preciousidubor@gmail.com>
2023-08-03 20:46:50 -0700

    Password  and hashed password validation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,11 @@ def hash_password(password: str) -> bytes:
     """Hashing"""
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Pasword hashing validation"""
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    else:
+        return Falsed
