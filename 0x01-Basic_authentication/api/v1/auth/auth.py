837e67d6bce0d6a34c102ec877000c72208f4b1b
Precious Idubor <preciousidubor@gmail.com>
2023-08-09 13:48:43 -0700

    Documentations added

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,10 +7,13 @@ from typing import List, TypeVar
 class Auth():
     """Authentication sys template"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns false"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """Auth header"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
         return None
