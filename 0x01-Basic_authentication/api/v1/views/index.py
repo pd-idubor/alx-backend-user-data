61fa4fcb85c373801886110e357d0bb300b9f1de
Precious Idubor <preciousidubor@gmail.com>
2023-08-09 13:11:40 -0700

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', strict_slashes=False)
+def unauth() -> str:
+    """Error handler"""
+    return abort(401)
