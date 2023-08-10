baac1b96104f0a09cd7ae0e848b5713b7af5564a
Precious Idubor <preciousidubor@gmail.com>
2023-08-10 09:30:00 -0700

    Decoding value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Defines BasicAuth class"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            de_str = base64.b64decode(base64_authorization_header)
+            return de_str.decode('utf-8')
+        except Exception:
+            return None
