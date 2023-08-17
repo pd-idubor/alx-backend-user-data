d420a0c7d50549e2b068f96539ca87d0a887d1f9
Precious Idubor <preciousidubor@gmail.com>
2023-08-17 14:34:14 -0700

    Get Session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,6 +32,16 @@ class Auth:
         except (NoResultFound):
             return False
 
+    def create_session(self, email) -> str:
+        """Returns the session ID as a string"""
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            self._db.update_user(user.id, session_id=uuid)
+            return uuid
+        except NoResultFound:
+            return
+
 
 def _hash_password(password: str) -> bytes:
     """Returns salted hash of the input password"""
