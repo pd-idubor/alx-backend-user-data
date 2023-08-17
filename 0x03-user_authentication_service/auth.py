46fa0cfc7e64b32b2564ba2fcff718ccd190da02
Precious Idubor <preciousidubor@gmail.com>
2023-08-17 13:22:47 -0700

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 """Hash password"""
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Save the user to the database using self._db
+        and return the User object"""
+        user = self._db._session.query(User).filter_by(email=email).first()
+        if user:
+            raise ValueError('User {} already exists'.format(email))
+        else:
+            hashed = _hash_password(password)
+            self._db.add_user(email, password)
+
 
 def _hash_password(password: str) -> bytes:
     """Returns salted hash of the input password"""
