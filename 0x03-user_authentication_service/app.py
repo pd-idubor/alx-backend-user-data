796f03544a043cb1410820564b0279eb2e968491
Precious Idubor <preciousidubor@gmail.com>
2023-08-17 13:56:33 -0700

    User registration

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from flask import Flask, jsonify
+from auth import Auth
+import requests
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +13,18 @@ app = Flask(__name__)
 def index() -> str:
     """Index page"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', method=['POST'], strict_slashes=False)
+def users() -> str:
+    """Register user"""
+    email = requests.request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
