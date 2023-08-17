cd3cb5ae7d5f8d2f267dd3b854a9220ba0ac77f6
Precious Idubor <preciousidubor@gmail.com>
2023-08-17 14:21:15 -0700

    Validation of credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,6 +23,14 @@ class Auth:
             hashed = _hash_password(password)
             return self._db.add_user(email, hashed)
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except (NoResultFound):
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """Returns salted hash of the input password"""
